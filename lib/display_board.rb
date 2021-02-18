96c945c6bd72c31d8bafdba4bd46bdd01b27cb12
David Ward <davidward.info@gmail.com>
2021-02-18 22:58:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | board[5] |   "
   puts "-----------"
   puts "   |   |   "
 end
